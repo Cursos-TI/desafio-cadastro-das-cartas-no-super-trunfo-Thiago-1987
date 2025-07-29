e43ecc8b621a07bdb0ea783fcc61efcfe57c59f2
Thiago-1987 <168478529+Thiago-1987@users.noreply.github.com>
2025-07-29 17:35:34 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int Carta, População, Pontos;
+    char Estado[12], Código[3], Nome_Cidade[10];
+    float Área, PIB;
 
+
+    printf(" Digite qual Número da Carta: ");
+    scanf("%i", &Carta);
+        
+    printf(" Digite qual Estado: ");
+    scanf("%1s", &Estado);
+
+    printf(" Digite o Código da Carta: ");
+    scanf("%s", &Código);
+
+    printf(" Digite o Nome da Cidade: ");
+    scanf("%s", &Nome_Cidade);
+    
+    printf(" Digite qual a População: ");
+    scanf("%i", &População);
+
+    printf(" Digite qual a Área em km2 : ");
+    scanf("%f", &Área);
+
+    printf(" Digite qual o PIB: ");
+    scanf("%f", &PIB);
+
+    printf(" Digite quantos Pontos tem a Carta: ");
+    scanf("%i", &Pontos);
+
+    printf(" Carta: %i \n Estado: %s \n Código: %s \n Cidade: %s \n População: %i milhoes\n Área: %f km2 \n", Carta, Estado, Código, Nome_Cidade, População, Área);
+    
+    printf(" PIB: %f bilhões \n Pontos: %i\n",PIB , Pontos);
+   
+
+    
     return 0;
 }
